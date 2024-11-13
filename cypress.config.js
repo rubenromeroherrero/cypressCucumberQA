f09684b0af61bdf772e8174fbdd1fa3bf1f2b273
RUBEN\ruben <ruben.romeroherrero@gmail.com>
2024-11-13 20:49:33 +0100

    add to broke the main test

--- cypress.config.js
+++ cypress.config.js
@@ -38,8 +38,8 @@ module.exports = defineConfig({
     },
     chromeWebSecurity: false,
     specPattern: ["**/*.feature", "**/apiTest/*/*.js"],
-    baseUrl: "https://www.saucedemo.com",
-    baseUrlPRE: "https://www.saucedemo-pre.com",
+    //baseUrl: "https://www.saucedemo.com",
+    //baseUrlPRE: "https://www.saucedemo-pre.com",
     //Esta configuración, es para cuando ejecutamos varios tests, solo guardamos el nº de test que queramos en memoria
     //numTestsKeptInMemory: 2,
     // wait => va a esperar todo el tiempo que le digas
@@ -52,54 +52,3 @@ module.exports = defineConfig({
     },
   },
 });
-
-// const {
-//   addCucumberPreprocessorPlugin,
-// } = require("@badeball/cypress-cucumber-preprocessor");
-// const {
-//   createEsbuildPlugin,
-// } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
-// const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-// const { defineConfig } = require("cypress");
-// const cypressOnFix = require("cypress-on-fix");
-
-// module.exports = defineConfig({
-//   reporter: "cypress-mochawesome-reporter",
-//   reporterOptions: {
-//     charts: true,
-//     reportPageTitle: "custom-title",
-//     embeddedScreenshots: true,
-//     inlineAssets: true,
-//     saveAllAttempts: false,
-//   },
-//   projectId: "ncsghx",
-//   e2e: {
-//     setupNodeEvents: async (on, config) => {
-//       // "cypress-on-fix" is required because "cypress-mochawesome-reporter" and "cypress-cucumber-preprocessor" use the same hooks
-//       on = cypressOnFix(on);
-
-//       // Mochawesome reporter plugin
-//       require("cypress-mochawesome-reporter/plugin")(on);
-
-//       // Cucumber plugin
-//       await addCucumberPreprocessorPlugin(on, config);
-
-//       // Esbuild preprocessor plugin
-//       on(
-//         "file:preprocessor",
-//         createBundler({ plugins: [createEsbuildPlugin(config)] })
-//       );
-
-//       return config;
-//     },
-//     failOnStatusCode: false,
-//     chromeWebSecurity: false,
-//     specPattern: ["**/*.feature", "**/apiTests/*/*.js"],
-//     defaultCommandTimeout: 10000,
-//     numTestsKeptInMemory: 10,
-//     env: {
-//       snapshotOnly: true,
-//       requestMode: true,
-//     },
-//   },
-// });
