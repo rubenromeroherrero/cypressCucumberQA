40af1235d25d04b0bafe9c7a6820dfca91a473e1
RUBEN\ruben <ruben.romeroherrero@gmail.com>
2024-11-13 20:43:50 +0100

    update fixes

--- cypress.config.js
+++ cypress.config.js
@@ -38,14 +38,6 @@ module.exports = defineConfig({
     },
     chromeWebSecurity: false,
     specPattern: ["**/*.feature", "**/apiTest/*/*.js"],
-    async setupNodeEvents(on, config) {
-      await addCucumberPreprocessorPlugin(on, config);
-      on(
-        "file:preprocessor",
-        createBundler({ plugins: [createEsbuildPlugin(config)] })
-      );
-      return config;
-    },
     baseUrl: "https://www.saucedemo.com",
     baseUrlPRE: "https://www.saucedemo-pre.com",
     //Esta configuración, es para cuando ejecutamos varios tests, solo guardamos el nº de test que queramos en memoria
